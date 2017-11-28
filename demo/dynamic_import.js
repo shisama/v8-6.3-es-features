188778157d404f15ae525eba2c5c8413ead7655c
Masashi Hirano <cherrydog07@gmail.com>
2017-11-29 00:30:22 +0900

    Dynamic import every 1 sec

--- demo/dynamic_import.js
+++ demo/dynamic_import.js
@@ -1,6 +1,12 @@
 (async() => {
-  for (let i = 1; i <= 3; i++) {
-    const module = await import(`./import_modules/hello${i}.js`);
-    module.hello();
-  }
+    let count = 0;
+    let id = setInterval(async() => {
+      const i = Math.floor(Math.random() * 3) + 1;
+      const module = await import(`./import_modules/hello${i}.js`);
+      module.hello();
+      count++;
+      if (count === 10) {
+        clearInterval(id);
+      }
+    }, 1000);
 })();
